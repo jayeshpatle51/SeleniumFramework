968885bf66c4a8ef40e8fccc3471b0daaf1c00c0
jayesh patle <jayesh_patle@grhombus.com>
2023-07-31 23:18:10 +0530

    new commit

---
+++ Myproject/Gitproject/src/git/Gitpro.java
@@ -0,0 +1,10 @@
+package git;
+
+public class Gitpro {
+
+	public static void main(String[] args) {
+		System.out.println("jayesh patle");
+
+	}
+
+}
---
+++ Myproject/Myproject1/src/Array.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		
+		}
+
+	}
+
